b4ab83f7d158cc678f1adbbae2d09fdef8318db5
JosephFouche <edex357@gmail.com>
2025-06-22 13:46:43 -0300

    Pequeño programa para escanear puertos en C

---
+++ PortScanner.c
@@ -0,0 +1,31 @@
+
+//target ip address ad port range
+// use sockets to check if a port is open
+//output: list of open ports and their associated services(eg: 80/HTTP, 22/ssh)
+
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main(){
+    int sock;
+    struct sockaddr_in target;
+    char ip[] = "127.0.0.1"; //replace with target ip
+    int start_port = 1, end_port = 100;
+
+    for(int port = start_port; port <= end_port; port++){
+        sock = socket(AF_INET, SOCK_STREAM,0);
+        target.sin_family = AF_INET;
+        target.sin_port = htons(port);
+        target.sin_addr.s_addr = inet_addr(ip);
+
+        if (connect(sock, (struct sockaddr*)&target, sizeof(target))==0){
+            printf("Port %d is open \n", port);
+        }
+        close(sock);
+    }
+    return 0;
+}
